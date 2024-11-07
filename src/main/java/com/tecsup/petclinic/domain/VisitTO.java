9ddb85b796292f91d54433cc94fb64397be0a69e
Tomas-coder-dev <tomasidk2004@gmail.com>
2024-11-07 14:46:27 -0500

    Correción del VisitTO

--- src/main/java/com/tecsup/petclinic/domain/VisitTO.java
+++ src/main/java/com/tecsup/petclinic/domain/VisitTO.java
@@ -1,19 +1,25 @@
 package com.tecsup.petclinic.domain;
 
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-/**
- * Transfer Object for Visit.
- */
-@Data
+
 @NoArgsConstructor
 @AllArgsConstructor
+@Data
 public class VisitTO {
 
     private Integer id;
+
     private Integer petId;
+
     private String visitDate;
+
     private String description;
+
+
 }

fc147466bae0c0f124fc85724ca8ab7f8132bcf6
Tomas-coder-dev <tomasidk2004@gmail.com>
2024-11-07 14:38:58 -0500

    Actualización de VisitTO

--- src/main/java/com/tecsup/petclinic/domain/VisitTO.java
+++ src/main/java/com/tecsup/petclinic/domain/VisitTO.java
@@ -1,4 +1,19 @@
 package com.tecsup.petclinic.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Transfer Object for Visit.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class VisitTO {
+
+    private Integer id;
+    private Integer petId;
+    private String visitDate;
+    private String description;
 }
